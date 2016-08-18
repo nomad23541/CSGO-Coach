c227d4d7adc307dda5e072f9bc621f74068c2d19
nomad23541 <chrisreading.email@gmail.com>
2016-08-18 17:38:58 -0600

    Added methods for loading the root layout

--- src/com/chrisreading/coach/MainApp.java
+++ src/com/chrisreading/coach/MainApp.java
@@ -1,6 +1,10 @@
 package com.chrisreading.coach;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -12,13 +16,43 @@ public class MainApp extends Application {
 	private Stage primaryStage;
 	private BorderPane rootLayout;
 	
+	/**
+	 * Get the main stage
+	 * @return The primary stage
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Coach");
 		
-		// init root layout
-		// build initial stage
+		initRootLayout();
+	}
+	
+	/**
+	 * Load & show the root scene
+	 */
+	public void initRootLayout() {
+		try {
+			// load rootlayout from it's file
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			// show the scene containing rootlayout
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 	
 }
