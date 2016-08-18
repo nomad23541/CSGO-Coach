6583c42fb08ce8a5554d0538c956e7e9f03de5ef
nomad23541 <chrisreading.email@gmail.com>
2016-08-18 17:24:21 -0600

    Added basic skeleton

--- src/com/chrisreading/coach/MainApp.java
+++ src/com/chrisreading/coach/MainApp.java
@@ -1,10 +1,24 @@
 package com.chrisreading.coach;
 
 import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  * Main class for coach
  */
-public class MainApp {
+public class MainApp extends Application {
+
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Coach");
+		
+		// init root layout
+		// build initial stage
+	}
 	
 }
