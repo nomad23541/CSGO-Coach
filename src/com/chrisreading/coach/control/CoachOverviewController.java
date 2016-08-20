b8233cee307e564dc4cfba4d0055221bd7b7e2d3
nomad23541 <chrisreading.email@gmail.com>
2016-08-20 14:03:25 -0600

    Added ability to transfer tasks between lists

--- src/com/chrisreading/coach/control/CoachOverviewController.java
+++ src/com/chrisreading/coach/control/CoachOverviewController.java
@@ -1,12 +1,14 @@
 package com.chrisreading.coach.control;
 
 import com.chrisreading.coach.MainApp;
+import com.chrisreading.coach.model.DeathmatchTask;
 import com.chrisreading.coach.model.Task;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 
 /**
  * Controller class for the CoachOverview
@@ -18,9 +20,9 @@ public class CoachOverviewController {
 	
 	/** FXML */
 	@FXML
-	private ListView<Task> taskList;
+	private ListView<Task> taskListDone;
 	@FXML
-	private ListView<Task> taskDragDropList;
+	private ListView<Task> taskList;
 	@FXML
 	private Button createButton;
 	@FXML
@@ -38,7 +40,41 @@ public class CoachOverviewController {
 	 */
 	@FXML
 	private void initialize() {
+		// temporary for testing
+		DeathmatchTask dt = new DeathmatchTask(4, 15);
+		DeathmatchTask dt2 = new DeathmatchTask(4, 23);
+		taskList.getItems().add(dt);
+		taskList.getItems().add(dt2);
 		
+		/**
+		 * Store tasks in the listviews but only show the
+		 * variable "title" as the label
+		 */
+		taskList.setCellFactory(param -> new ListCell<Task>() {
+			@Override
+			protected void updateItem(Task item, boolean empty) {
+				super.updateItem(item, empty);
+				
+				if(empty || item == null || item.getTitle() == null) {
+					setText(null);
+				} else {
+					setText(item.getTitle());
+				}
+			}
+		});
+		
+		taskListDone.setCellFactory(param -> new ListCell<Task>() {
+			@Override
+			protected void updateItem(Task item, boolean empty) {
+				super.updateItem(item, empty);
+				
+				if(empty || item == null || item.getTitle() == null) {
+					setText(null);
+				} else {
+					setText(item.getTitle());
+				}
+			}
+		});
 	}
 	
 	/**
@@ -63,6 +99,20 @@ public class CoachOverviewController {
 	 */
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
+	}
+	
+	/**
+	 * When the task list is double clicked, 
+	 * bring the selected task to the task list done
+	 * listview
+	 */
+	public void onTaskListClicked(MouseEvent event) {	
+		// if 2 clicks
+		if(event.getClickCount() == 2) {
+			Task task = taskList.getSelectionModel().getSelectedItem();
+			// now add that task to the done list
+			taskListDone.getItems().add(task);
+		}
 	}
 
 }
