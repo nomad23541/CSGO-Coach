acc83c584aeb1f55f5b24796a412d516be76de49
nomad23541 <chrisreading.email@gmail.com>
2016-08-20 13:40:45 -0600

    Created Deathmatch model

---
+++ src/com/chrisreading/coach/model/DeathmatchTask.java
@@ -0,0 +1,52 @@
+package com.chrisreading.coach.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+/**
+ * Deathmatch model task
+ */
+public class DeathmatchTask extends Task {
+	
+	private IntegerProperty deaths, kills;
+	
+	/**
+	 * Default Constructor
+	 */
+	public DeathmatchTask() {
+		this(0, 0);
+	}
+	
+	/**
+	 * Customizable constructor
+	 * @param deaths
+	 * @param kills
+	 */
+	public DeathmatchTask(int deaths, int kills) {
+		this.setTitle("Deathmatch"); // set title as this is a default task
+		
+		this.deaths = new SimpleIntegerProperty();
+		this.kills = new SimpleIntegerProperty();
+	}
+	
+	/**
+	 * GETTER SETTERS
+	 */
+	
+	public void setDeaths(int deaths) {
+		this.deaths.set(deaths);
+	}
+	
+	public int getDeaths() {
+		return deaths.get();
+	}
+	
+	public void setKills(int kills) {
+		this.kills.set(kills);
+	}
+	
+	public int getKills() {
+		return kills.get();
+	}
+
+}
