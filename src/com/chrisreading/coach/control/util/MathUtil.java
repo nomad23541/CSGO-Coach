a64f393a482806f6ddf084e11a7fee8a26ce5a31
nomad23541 <chrisreading.email@gmail.com>
2016-08-21 01:11:35 -0600

    Added MathUtil for all math operations in Coach

---
+++ src/com/chrisreading/coach/control/util/MathUtil.java
@@ -0,0 +1,19 @@
+package com.chrisreading.coach.control.util;
+
+/**
+ * Utility class for handling all
+ * equations used.
+ */
+public class MathUtil {
+
+	/**
+	 * Gets the kill/death ratio 
+	 * @param k kills
+	 * @param d deaths
+	 * @return Solved kd
+	 */
+	public static double getKDRatio(int k, int d) {
+		return k / d;
+	}
+	
+}
