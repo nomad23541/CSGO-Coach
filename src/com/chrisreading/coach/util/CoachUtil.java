386827df4a84c479e2b803b52cded33c128625cb
nomad23541 <chrisreading.email@gmail.com>
2016-08-28 00:34:17 -0600

    Added maps array

--- src/com/chrisreading/coach/util/CoachUtil.java
+++ src/com/chrisreading/coach/util/CoachUtil.java
@@ -14,5 +14,7 @@ public class CoachUtil {
 	public final static String[] rifles = new String[] {"AK-47","AUG","AWP","FAMAS","Galil AR","G3SG1","M4A1-S","M4A4","SCAR-20","SG 553","SSG 08"};
 	public final static String[] grenades = new String[] {"Decoy Grenade","Flashbang","HE Grenade","Incendiary Grenade","Molotov","Smoke Grenade"};
 	public final static String[] gear = new String[] {"C4 Charge","Defusal Kit","Kevlar","Kevlar and Helmet","Zeus x27"};
+	
+	public final static String[] maps = new String[] {"de_aztec","de_dust","de_dust2","de_inferno","de_nuke","de_train","cs_italy","cs_office","de_cbble","de_mirage","de_overpass","de_vertigo","cs_assault","cs_militia","ar_baggage","ar_shoots","de_bank","de_lake","de_safehouse","de_shorttrain","de_stmarc","de_sugarcane","ar_monastery","de_dust2_se","de_inferno_se","de_train_se","de_nuke_se","de_mirage_csgo","de_dust_se","de_mill_b2"};
 
 }
