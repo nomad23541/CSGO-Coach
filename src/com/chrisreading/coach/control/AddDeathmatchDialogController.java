702d26b3ed9d3c57889c7daffc652f3d45fc88df
nomad23541 <chrisreading.email@gmail.com>
2016-08-28 01:46:54 -0600

    Changed textfield to combobox for guns

--- src/com/chrisreading/coach/control/AddDeathmatchDialogController.java
+++ src/com/chrisreading/coach/control/AddDeathmatchDialogController.java
@@ -1,8 +1,10 @@
 package com.chrisreading.coach.control;
 
 import com.chrisreading.coach.model.DeathmatchTask;
+import com.chrisreading.coach.util.CoachUtil;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 /**
@@ -12,7 +14,7 @@ public class AddDeathmatchDialogController extends DialogController {
 	
 	/** FXML */
 	@FXML
-	private TextField gunField;
+	private ComboBox<String> gunComboBox;
 	@FXML
 	private TextField timeField;
 	@FXML
@@ -27,6 +29,20 @@ public class AddDeathmatchDialogController extends DialogController {
 	@FXML
 	protected void initialize() {
 		task = new DeathmatchTask();
+		
+		// add guns to the combobox
+		for(String gun : CoachUtil.heavy) {
+			gunComboBox.getItems().add(gun);
+		}
+		for(String gun : CoachUtil.pistols) {
+			gunComboBox.getItems().add(gun);
+		}
+		for(String gun : CoachUtil.rifles) {
+			gunComboBox.getItems().add(gun);
+		}
+		for(String gun : CoachUtil.smgs) {
+			gunComboBox.getItems().add(gun);
+		}
 	}
 	
 	/**
@@ -37,7 +53,7 @@ public class AddDeathmatchDialogController extends DialogController {
 		((DeathmatchTask) task).setKills(Integer.parseInt(killsField.getText()));
 		((DeathmatchTask) task).setAssists(Integer.parseInt(assistsField.getText()));
 		((DeathmatchTask) task).setDeaths(Integer.parseInt(deathsField.getText()));
-		((DeathmatchTask) task).setGun(gunField.getText());
+		((DeathmatchTask) task).setGun(gunComboBox.getSelectionModel().getSelectedItem());
 		((DeathmatchTask) task).setTime(timeField.getText());
 		
 		okClicked = true;
