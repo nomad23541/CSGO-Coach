75f3037a100fc33681d252a4a775fd1a5f15a2ad
nomad23541 <chrisreading.email@gmail.com>
2016-08-28 00:32:10 -0600

    Contains all weapons and gear in CS:GO

---
+++ src/com/chrisreading/coach/util/CoachUtil.java
@@ -0,0 +1,18 @@
+package com.chrisreading.coach.util;
+
+/**
+ * Contains lists of
+ * all weapons and maps in CS:GO
+ */
+public class CoachUtil {
+	
+	public final static String melee = "Knife";
+	
+	public final static String[] pistols = new String[] {"CZ75-Auto","Desert Eagle","Dual Berettas","Five-SeveN","Glock-18","P250","P2000","R8 Revolver","Tec-9","USP-S"};
+	public final static String[] heavy = new String[] {"MAG-7","M249","Nova","Negev","Sawed-Off","XM1014"};
+	public final static String[] smgs = new String[] {"MAC-10","MP9","MP7","P90","PP-Bizon","UMP-45"};
+	public final static String[] rifles = new String[] {"AK-47","AUG","AWP","FAMAS","Galil AR","G3SG1","M4A1-S","M4A4","SCAR-20","SG 553","SSG 08"};
+	public final static String[] grenades = new String[] {"Decoy Grenade","Flashbang","HE Grenade","Incendiary Grenade","Molotov","Smoke Grenade"};
+	public final static String[] gear = new String[] {"C4 Charge","Defusal Kit","Kevlar","Kevlar and Helmet","Zeus x27"};
+
+}
