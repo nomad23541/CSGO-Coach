2865d0670dc69f908b49e0d97f616c73ef596d5c
nomad23541 <chrisreading.email@gmail.com>
2016-08-28 01:47:33 -0600

    Filled in skeleton

--- src/com/chrisreading/coach/control/AddGrenadeTrainingDialogController.java
+++ src/com/chrisreading/coach/control/AddGrenadeTrainingDialogController.java
@@ -1,9 +1,18 @@
 package com.chrisreading.coach.control;
 
+import java.io.File;
+
+import com.chrisreading.coach.model.GrenadeTrainingTask;
+import com.chrisreading.coach.util.CoachUtil;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 /**
  * Controller class for the GrenadeTraining Dialog
@@ -29,22 +38,63 @@ public class AddGrenadeTrainingDialogController extends DialogController {
 	private Button attachButton;
 	@FXML
 	private TextField imageLocField;
+	@FXML
+	private ImageView imageView;
 	
 	public AddGrenadeTrainingDialogController() {}
 
 	@Override
 	protected void initialize() {
-
+		task = new GrenadeTrainingTask();
+		
+		imageLocField.setEditable(false);
+		
+		// add all maps to combobox
+		for(String map : CoachUtil.maps) {
+			mapComboBox.getItems().add(map);
+		}
+		
+		// add all grenades to combobox
+		for(String nade : CoachUtil.grenades) {
+			nadeComboBox.getItems().add(nade);
+		}
+	}
+	
+	/** 
+	 * Open filechooser and find image
+	 */
+	@FXML
+	private void handleAttach() {
+		FileChooser fc = new FileChooser();
+		FileChooser.ExtensionFilter filter = new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif", "*.bmp");
+		fc.setSelectedExtensionFilter(filter);
+		fc.setTitle("Attach an image");
+		File file = fc.showOpenDialog(dialogStage);
+		
+		if(file != null) {
+			((GrenadeTrainingTask) task).setImage(new Image(file.toURI().toString(), true));
+			imageView.setImage(((GrenadeTrainingTask) task).getImage());
+			imageLocField.setText(file.getAbsolutePath());
+		}
+		
 	}
 
 	@Override
 	protected void handleOk() {
-
+		((GrenadeTrainingTask) task).setGrenade(nadeComboBox.getSelectionModel().getSelectedItem());
+		((GrenadeTrainingTask) task).setMap(mapComboBox.getSelectionModel().getSelectedItem());
+		((GrenadeTrainingTask) task).setScoreATop(Integer.parseInt(scoreATop.getText()));
+		((GrenadeTrainingTask) task).setScoreBTop(Integer.parseInt(scoreBTop.getText()));
+		((GrenadeTrainingTask) task).setScoreABot(Integer.parseInt(scoreABot.getText()));
+		((GrenadeTrainingTask) task).setScoreBBot(Integer.parseInt(scoreBBot.getText()));
+		
+		dialogStage.close();
+		okClicked = true;
 	}
 
 	@Override
 	protected void handleCancel() {
-
+		dialogStage.close();
 	}
 
 }
